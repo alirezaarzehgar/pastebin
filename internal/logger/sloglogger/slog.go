b74ce40d0bfa0f9e9d60346fab15a5c738d921ea
Alireza Arzehgar <alirezaarzehgar82@gmail.com>
2026-02-11 17:37:31 +0330

    logger: fix log level for slog logger
    Signed-off-by: Alireza Arzehgar <alirezaarzehgar82@gmail.com>

--- internal/logger/sloglogger/slog.go
+++ internal/logger/sloglogger/slog.go
@@ -2,58 +2,64 @@ package sloglogger
 
 import (
 	"log/slog"
+	"os"
 
 	"github.com/alirezaarzehgar/pastebin/internal/logger"
 )
 
 type slogLogger struct {
-	level logger.Leveler
+	logger *slog.Logger
 }
 
 func (l slogLogger) Error(msg string, args ...any) {
-	slog.Error(msg, args...)
+	l.logger.Error(msg, args...)
 }
 
 func (l slogLogger) Warn(msg string, args ...any) {
-	if l.level > logger.LevelWarn {
-		return
-	}
-	slog.Warn(msg, args...)
-
+	l.logger.Warn(msg, args...)
 }
 
 func (l slogLogger) Info(msg string, args ...any) {
-	if l.level > logger.LevelInfo {
-		return
-	}
-	slog.Info(msg, args...)
+	l.logger.Info(msg, args...)
 }
 
 func (l slogLogger) Debug(msg string, args ...any) {
-	if l.level > logger.LevelDebug {
-		return
-	}
-	slog.Debug(msg, args...)
+	l.logger.Debug(msg, args...)
 }
 
 func (l slogLogger) Verbose(msg string, args ...any) {
-	if l.level > logger.LevelVerbose {
-		return
-	}
-	slog.Debug(msg, args...)
+	l.logger.Debug(msg, args...)
 }
 
 type Options func(l *slogLogger)
 
 func OptionSetLogLevel(level logger.Leveler) Options {
+	var slogLevel slog.Level
+	switch level {
+	case logger.LevelError:
+		slogLevel = slog.LevelError
+	case logger.LevelWarn:
+		slogLevel = slog.LevelWarn
+	case logger.LevelInfo:
+		slogLevel = slog.LevelInfo
+	case logger.LevelDebug:
+		slogLevel = slog.LevelDebug
+	case logger.LevelVerbose:
+		slogLevel = slog.LevelDebug - 2
+	default:
+		slogLevel = slog.LevelInfo
+	}
+
 	return func(l *slogLogger) {
-		l.level = level
+		slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slogLevel,
+		})
 	}
 }
 
 func New(opts ...Options) logger.Logger {
 	l := &slogLogger{
-		level: logger.LevelInfo,
+		logger: slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{})),
 	}
 
 	for _, opt := range opts {
