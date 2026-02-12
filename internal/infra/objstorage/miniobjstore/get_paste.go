a7bf5d9d7b3370fc5e48f1b4b1b2134cbecc8c7e
Alireza Arzehgar <alirezaarzehgar82@gmail.com>
2026-02-12 14:30:55 +0330

    feat: enable lazy expiration for obtaining objects
    Signed-off-by: Alireza Arzehgar <alirezaarzehgar82@gmail.com>

--- internal/infra/objstorage/miniobjstore/get_paste.go
+++ internal/infra/objstorage/miniobjstore/get_paste.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"time"
 
 	"github.com/alirezaarzehgar/pastebin/internal/domain/model"
 	"github.com/alirezaarzehgar/pastebin/internal/infra/objstorage"
@@ -20,6 +21,15 @@ func (m *minIO) DownloadOject(ctx context.Context, args model.GetPasteObjectArgs
 		}
 		logger.Error("failed to get object", "error", err, "bucket", args.PasteID, "filename", args.Filename)
 		return nil, fmt.Errorf("failed to get object: %w", err)
+	}
+
+	stat, err := obj.Stat()
+	if err != nil {
+		return nil, fmt.Errorf("getting object stat failed: %w", err)
+	}
+
+	if time.Now().After(stat.Expires) {
+		return nil, objstorage.NotFound
 	}
 
 	reply := model.GetPasteObjectReply{
